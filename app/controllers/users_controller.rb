fe093b3277a731949136f175947b3580152f2376
MisterAnthropy <tjcroley@outlook.com>
2020-07-30 15:25:32 -0700

    bundled bcrypt, successfully displayed  signup page after creating account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.user.id 
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     end
 
     def show
-
+        @user = User.find_by(id: params[:id])
     end
 
     private
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :equalizers
     has_many :saved_artists, through: :equalizers, source: :artist 
     has_many :artists 
+    has_secure_password 
 end
