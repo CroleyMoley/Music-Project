3517923bc6b5cedc4ca97b5e4752027925cac99f
MisterAnthropy <tjcroley@outlook.com>
2020-07-30 15:17:55 -0700

    added actions to the users controller t

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,23 @@ class UsersController < ApplicationController
     end
 
     def create 
-        @user = User.create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.user.id 
+            redirect_to user_path(@user)
+        else
+            render :new
+        end  
+    end
+
+    def show
+
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
     end
 
 end
