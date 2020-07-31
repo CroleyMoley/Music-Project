2d2083241353981d8d5d2c4a391065cd4b6a4d59
MisterAnthropy <tjcroley@outlook.com>
2020-07-31 15:48:36 -0700

    hid the logout blink from users that are not logged in. added Create New Artist link.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else 
-            flash[:error] = "Login Credentials are Invaldid. Please Try Again."
+            flash[:error] = "Login Credentials are Invalid. Please Try Again."
             redirect_to login_path
         end
     end
