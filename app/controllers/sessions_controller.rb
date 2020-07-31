b22b7b1240bdded63d97919b301e4633b7f3cea9
MisterAnthropy <tjcroley@outlook.com>
2020-07-31 15:39:57 -0700

    added security for user logins

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,19 @@ class SessionsController < ApplicationController
 
     def new 
     end
-    
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+
+        if @user.try(:authenticate, params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else 
+            flash[:error] = "Login Credentials are Invaldid. Please Try Again."
+            redirect_to login_path
+        end
+    end
+
 
     def destroy
         session.delete(:user_id)
