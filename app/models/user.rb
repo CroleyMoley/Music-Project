b5d2104682335671c86fdd9edfd043afde8f23ab
MisterAnthropy <tjcroley@outlook.com>
2020-07-31 14:00:09 -0700

    validates username for uniqueness and presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :equalizers
     has_many :saved_artists, through: :equalizers, source: :artist 
     has_many :artists 
+
+    validates :username, uniqueness: true, presence: true
     has_secure_password 
 end
