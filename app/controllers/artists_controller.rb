c42ed8f0ce0ade140495e4131f085389825229f4
MisterAnthropy <tjcroley@outlook.com>
2020-08-02 15:19:48 -0700

    added set_artist action to controller and update method

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+    before_action :set_artist, only:[:show, :edit, :update]
 
     def new 
         @artist = Artist.new 
@@ -9,6 +10,8 @@ class ArtistsController < ApplicationController
         @artist = Artist.new(artist_params)
         @artist.user_id = session[:user_id]
         if @artist.save
+            @artist.image.purge
+            @artist.image.attach(params[:artist][:image])
             redirect_to artist_path(@artist)
         else
             @artist.build_genre
@@ -27,8 +30,24 @@ class ArtistsController < ApplicationController
     def edit
     end
 
+    def update
+        if @artist.update(artist_params)
+            @artist.image.purge
+            @artist.image.attach(params[:artist][:image])
+            redirect_to artist_path(@artist)
+        else
+            render :edit
+        end
+    end
 
+    private
     def artist_params
         params.require(:artist).permit(:artist_name, :genre_id, genre_attributes: [:name])
     end
+
+    def set_artist
+        @artist = Artist.find_by(params[:id])
+        redirect_to artists_path if !@artist
+    end
+
 end
