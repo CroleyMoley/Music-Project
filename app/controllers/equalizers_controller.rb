6b0e0c6eddecfd2996ba05b4bcaf919093640d2c
MisterAnthropy <tjcroley@outlook.com>
2020-08-03 17:53:45 -0700

    wrote out all controller actions for requalizers

--- app/controllers/equalizers_controller.rb
+++ app/controllers/equalizers_controller.rb
@@ -1,11 +1,36 @@
 class EqualizersController < ApplicationController
 
     def new
-        @artist = Artist.find_by(id: params[:id])
-        @equalizer = @artist.build_equalizer
+        if @artist = Artist.find_by_id(params[:artist_id])
+            @equalizer = @artist.equalizers.build
+        else
+            @equalizer = Equalizer.new
+        end
     end
 
     def index
+        if @artist = Artist.find_by_id(params[:artist_id])
+            @equalizers = @artist.equalizers
+        else
+            @equalizers = Equalizer.all
+        end
+    end
 
+    def create
+        @equalizer = current_user.equalizers.build(equalizer_params)
+        if @equalizer.save
+            redirect_to equalizer_path(@equalizer)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @equalizer = Equalizer.find_by(id: params[:id])
+    end
+
+    private
+    def equalizer_params
+        params.require(:equalizer).permit(:artist_id, :settings)
     end
 end
