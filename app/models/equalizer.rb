2b5bc31291251ebf5a6ffc1073134a388ffd6a27
MisterAnthropy <tjcroley@outlook.com>
2020-08-07 15:11:13 -0700

    added validations to models

--- app/models/equalizer.rb
+++ app/models/equalizer.rb
@@ -3,4 +3,5 @@ class Equalizer < ApplicationRecord
   belongs_to :artist
 
   validates :settings, presence: true
+  validates :artist, uniqueness: { scope: :user, message: "already has an EQ Save State" }
 end
