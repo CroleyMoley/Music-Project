92b129277057f22d72ef5bcd1dc333547a0dd834
MisterAnthropy <tjcroley@outlook.com>
2020-08-07 15:30:54 -0700

    added more validations and fixed error displays

--- app/helpers/equalizers_helper.rb
+++ app/helpers/equalizers_helper.rb
@@ -1,2 +1,10 @@
 module EqualizersHelper
+
+    def display_header(equalizer)
+        if params[:artist_id]
+            content_tag(:h1, "Add an EQ Save State for #{equalizer.artist.artist_name} - #{equalizer.artist.genre.name}")
+        else
+            content_tag(:h1, "Create New Equalizer Settings")
+        end
+    end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,10 +3,30 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :equalizers
   has_many :users, through: :equalizers
-  accepts_nested_attributes_for :genre
+  
+
+  validates :artist_name, presence: true, uniqueness: true
+  validates :no_duplicates
+
+  def self.alpha
+    order(:artist_name)
+  end
+
 
   def artist_attributes=(attributes)
     artist = Artist.find_or_create_by(attributes) if !attributes['name'].empty?
   end
+
+  def no_duplicates 
+    if Artist.find_by(artist_name: artist_name, genre_id: genre_id)
+      errors.add(:artist_name, 'is already listed in the genre')
+    end
+  end
+
+  def artist_name_and_genre
+    "#{artist_name} - #{genre.name}"
+  end
+  
+
   
 end
