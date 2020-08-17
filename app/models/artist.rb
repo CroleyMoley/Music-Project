ac43d18f961eb65a95b3f1df5150a32c1d451fe3
MisterAnthropy <tjcroley@outlook.com>
2020-08-17 12:22:12 -0700

    removed no_duplicate method in place on reject_if validation

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,11 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :equalizers
   has_many :users, through: :equalizers
-  accepts_nested_attributes_for :genre
+  accepts_nested_attributes_for :genre, reject_if: :all_blank
   
 
   validates :artist_name, presence: true, uniqueness: true
-  validate :no_duplicate
+  #validate :no_duplicate
 
   
 
@@ -20,11 +20,11 @@ class Artist < ApplicationRecord
     artist = Artist.find_or_create_by(attributes) if !attributes['name'].empty?
   end
 
-  def no_duplicate 
-    if Artist.find_by(artist_name: artist_name, genre_id: genre_id)
-      errors.add(:artist_name, 'is already listed in the genre')
-    end
-  end
+  # def no_duplicate 
+  #   if Artist.find_by(artist_name: artist_name, genre_id: genre_id)
+  #     errors.add(:artist_name, 'is already listed in the genre')
+  #   end
+  # end
 
   def artist_name_and_genre
     "#{artist_name} - #{genre.name}"
